73a7f81a7a5f4ed8a14d17665101d5c59ec64f9e
Acer <pyandakhan24@gmail.com>
2022-04-12 15:55:47 +0500

    assignment 1

---
+++ app/src/main/java/com/example/sp20_bse_032_ass_01/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.sp20_bse_032_ass_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
